73bee7337dd5207ee2d0bffe016daf1bc16e1b19
lbjcena <lbjcena@gmail.com>
2026-03-02 11:08:04 +0100

    NavBar - pathnames added, wired to conditional styling of nav buttons

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,19 +1,21 @@
 'use client'
 
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 const NavBar = () => {
+  const pathname = usePathname()
   return (
     <nav className="w-screen bg-gray-400 p-2">
       <ul className="flex justify-center gap-4">
         <li>
-          <Link className="w-full bg-gray-600 text-gray-100 px-4 py-1" href={"/"}>Main screen</Link>
+          <Link className={`w-full bg-gray-600 text-gray-100 px-4 py-1 hover:bg-blue-600 duration-125 rounded-sm font-medium ${pathname === "/" ? "bg-green-700" : ""}`} href={"/"}>Main screen</Link>
         </li>
         <li>
-          <Link className="w-full bg-gray-600 text-gray-100 px-4 py-1" href={"/hi-score"}>Leaderboard</Link>
+          <Link className={`w-full bg-gray-600 text-gray-100 px-4 py-1 hover:bg-blue-600 duration-125 rounded-sm font-medium ${pathname === "/hi-score" ? "bg-green-700" : ""}`} href={"/hi-score"}>Leaderboard</Link>
         </li>
         <li>
-          <Link className="w-full bg-gray-600 text-gray-100 px-4 py-1" href={"/about"}>About us</Link>
+          <Link className={`w-full bg-gray-600 text-gray-100 px-4 py-1 hover:bg-blue-600 duration-125 rounded-sm font-medium ${pathname === "/about" ? "bg-green-700" : ""}`} href={"/about"}>About us</Link>
         </li>
       </ul>
     </nav>
