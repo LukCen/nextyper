ada0b7ec82143f84c7ea7007b8441c34e5ab7af2
lbjcena <lbjcena@gmail.com>
2026-03-02 11:03:25 +0100

    NavBar - basic markup

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,22 @@
 'use client'
 
+import Link from "next/link"
+
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className="w-screen bg-gray-400 p-2">
+      <ul className="flex justify-center gap-4">
+        <li>
+          <Link className="w-full bg-gray-600 text-gray-100 px-4 py-1" href={"/"}>Main screen</Link>
+        </li>
+        <li>
+          <Link className="w-full bg-gray-600 text-gray-100 px-4 py-1" href={"/hi-score"}>Leaderboard</Link>
+        </li>
+        <li>
+          <Link className="w-full bg-gray-600 text-gray-100 px-4 py-1" href={"/about"}>About us</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
