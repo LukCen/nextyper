1642bff1463e0a45bf7f817310e5aa15ffd81b04
lbjcena <lbjcena@gmail.com>
2026-03-02 11:31:29 +0100

    menuscreen displayed conditionally - wiring it now

--- app/components/MenuScreen.tsx
+++ app/components/MenuScreen.tsx
@@ -1,7 +1,24 @@
 'use client'
+
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+interface MenuscreenProps { }
+
 const MenuScreen = () => {
   return (
-    <div>MenuScreen</div>
+    <main className="w-fit mx-auto">
+      <h1>Welcome to NexTyper! Input your name below and press start to begin.</h1>
+      <Field className=" bg-slate-200 shadow-sm p-2 mt-4">
+        <FieldLabel htmlFor="input-demo-api-key">Your player name</FieldLabel>
+        <Input id="input-demo-api-key" type="text" placeholder="John Doe" />
+        <FieldDescription>
+          Your selected name will persist through sessions.
+        </FieldDescription>
+        <button className="px-4 py-1 bg-blue-500 rounded-sm max-w-fit">Start</button>
+      </Field>
+    </main>
+
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 'use client'
+
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
+import MenuScreen from "./components/MenuScreen";
+
 export default function Home() {
+  const [gameStarted, setGameStarted] = useState(false)
+  const [playerName, setPlayerName] = useState("")
   return (
-    <main>
-      Nextyper Test
-    </main>
+    <>
+      {gameStarted ? <GameBoard /> : <MenuScreen />}
+    </>
   );
 }
