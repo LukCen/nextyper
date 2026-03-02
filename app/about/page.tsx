e722142aba97f7da484ea9adcada279937376d04
lbjcena <lbjcena@gmail.com>
2026-03-02 11:12:45 +0100

    about page - some basic info and styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,21 @@
 
 const page = () => {
   return (
-    <div>About goes here</div>
+    <main className="flex flex-col gap-4 items-center">
+      <section className="baseinfo flex flex-col gap-2 items-center">
+        <h1>NexTyper - a typing game/exercise for practicing your speed and accuracy!</h1>
+        <p>NexTyper is a web application developed for purpsoe of testing your speed and accuracy when typing.</p>
+        <p>Test out your skills and see if you can beat the hall of fame!</p>
+      </section>
+      <section className="techstack">
+        <h2>Developed using:</h2>
+        <ul className="flex flex-col gap-2">
+          <li>Next.js - main framework</li>
+          <li>Jest - unit tests</li>
+          <li>ShadCN - some components (tables for the hi-score page mostly)</li>
+        </ul>
+      </section>
+    </main>
   )
 }
 
