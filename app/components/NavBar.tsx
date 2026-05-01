2d615184426e335305fccdf5753725c6475eaad9
lbjcena <lbjcena@gmail.com>
2026-05-02 00:22:11 +0200

    icons and minor layout change to navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,21 +1,32 @@
 'use client'
 
+import { House, Info, Trophy } from "lucide-react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
 const NavBar = () => {
   const pathname = usePathname()
   return (
-    <nav className="w-screen bg-gray-400 p-2">
-      <ul className="flex justify-center gap-4">
-        <li>
-          <Link className={`w-full bg-gray-600 text-gray-100 px-4 py-1 hover:bg-blue-600 duration-125 rounded-sm font-medium ${pathname === "/" ? "bg-green-700" : ""}`} href={"/"}>Main screen</Link>
+    <nav className="flex items-center justify-around w-screen bg-gray-400 p-2">
+      <span className="font-black text-4xl">NexTyper</span>
+      <ul className="flex justify-center gap-8">
+        <li className="flex items-center justify-center">
+          <Link className={`flex items-center justify-center  gap-2 w-full bg-gray-600 text-gray-100 px-4 py-1 hover:bg-blue-600 duration-125 rounded-sm font-medium ${pathname === "/" ? "bg-green-700" : ""}`} href={"/"}>
+            Main screen
+            <House size={20} />
+          </Link>
         </li>
-        <li>
-          <Link className={`w-full bg-gray-600 text-gray-100 px-4 py-1 hover:bg-blue-600 duration-125 rounded-sm font-medium ${pathname === "/hi-score" ? "bg-green-700" : ""}`} href={"/hi-score"}>Leaderboard</Link>
+        <li className="flex items-center justify-center">
+          <Link className={`flex items-center justify-center  gap-2 w-full bg-gray-600 text-gray-100 px-4 py-1 hover:bg-blue-600 duration-125 rounded-sm font-medium ${pathname === "/hi-score" ? "bg-green-700" : ""}`} href={"/hi-score"}>
+            Leaderboard
+            <Trophy size={20} />
+          </Link >
         </li>
-        <li>
-          <Link className={`w-full bg-gray-600 text-gray-100 px-4 py-1 hover:bg-blue-600 duration-125 rounded-sm font-medium ${pathname === "/about" ? "bg-green-700" : ""}`} href={"/about"}>About us</Link>
+        <li className="flex items-center justify-center">
+          <Link className={`flex items-center justify-center  gap-2 w-full bg-gray-600 text-gray-100 px-4 py-1 hover:bg-blue-600 duration-125 rounded-sm font-medium ${pathname === "/about" ? "bg-green-700" : ""}`} href={"/about"}>
+            About us
+            <Info size={20} />
+          </Link>
         </li>
       </ul>
     </nav>
