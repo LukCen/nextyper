b4af3ff315f6e9bdce8dfb64c0499f5b8b3d0611
lbjcena <lbjcena@gmail.com>
2026-05-04 00:54:21 +0200

    css cleanups - removed @apply directives, added a color sliding styling on the mainmenu button

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,16 +5,16 @@ const page = () => {
     <main className="flex flex-col gap-4 items-center w-fit mx-auto">
       <section className="baseinfo flex flex-col gap-2 items-center">
         <h1 className="text-2xl font-bold">NexTyper - a typing game for practicing your speed and accuracy!</h1>
-        <p>NexTyper is a web application developed for purpsoe of testing your speed and accuracy when typing.</p>
+        <p>NexTyper is a web game developed for purpsoe of testing your speed and accuracy when typing.</p>
         <p>Test out your skills and see if you can beat the hall of fame!</p>
       </section>
       <hr className="border-neon-green w-full" />
       <section className="techstack flex flex-col gap-2 items-center">
-        <h2>Developed using:</h2>
-        <ul className="flex flex-col gap-2">
+        <h2 className="text-xl font-bold">Developed using:</h2>
+        <ul className="flex flex-col gap-2 text-center">
           <li>Next.js - main framework</li>
           <li>Jest - unit tests</li>
-          <li>ShadCN - some components (tables for the hi-score page mostly)</li>
+          <li>ShadCN - some components</li>
         </ul>
       </section>
     </main>
--- app/components/MenuScreen.tsx
+++ app/components/MenuScreen.tsx
@@ -2,6 +2,7 @@
 
 import { Field, FieldDescription, FieldLabel } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { ArrowRight } from "lucide-react"
 import { useState } from "react"
 
 interface MenuScreenProps {
@@ -13,16 +14,16 @@ const MenuScreen = ({ onStart, onNameSet }: MenuScreenProps) => {
   const [name, setName] = useState("")
   return (
     <main className="w-fit mx-auto">
-      <h1>Welcome to NexTyper! Input your name below and press start to begin.</h1>
+      <h1 className="text-2xl font-bold text-center">Welcome to NexTyper!<br /> Input your name below and press start to begin.</h1>
       <Field className="shadow-sm p-2 mt-4">
         <FieldLabel htmlFor="input-demo-api-key">Your player name</FieldLabel>
         <Input value={name} onChange={(e) => setName(e.target.value)} id="username" type="text" placeholder="John Doe" />
         <FieldDescription>
           Your selected name will persist through sessions.
         </FieldDescription>
-        <button onClick={() => { onNameSet(name), onStart() }} className="px-4 py-1 bg-blue-500 rounded-sm max-w-fit">Start</button>
+        <button onClick={() => { onNameSet(name), onStart() }} className="flex justify-center items-center px-4 py-1 color-slide rounded-sm max-w-fit">Start <ArrowRight size={16} /> </button>
       </Field>
-    </main>
+    </main >
 
   )
 }
