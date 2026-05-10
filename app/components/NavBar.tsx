0575fafa2ac73a480826c588cdac5ad74d3fc74b
lbjcena <lbjcena@gmail.com>
2026-05-10 16:58:08 +0200

    mobile menu added wit button

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,6 +3,7 @@
 import { House, Info, Trophy } from "lucide-react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { useState } from "react"
 
 const links =
   [
@@ -15,20 +16,41 @@ const links =
 
 const NavBar = () => {
   const pathname = usePathname()
+  const [menuIsVisible, setMenuIsVisible] = useState(false);
   return (
-    <nav className="flex items-center justify-around w-screen border-b-1 border-neon-green p-2">
-      <span className="font-black text-4xl">NexTyper</span>
-      <ul className="flex justify-center gap-8">
-        {links.map(({ text, pathName, href, icon: Icon, key }) =>
-          <li className="flex items-center justify-center" key={key}>
-            <Link className={`flex items-center text-background justify-center gap-2 px-4 py-1 rounded-sm font-medium ${pathname === pathName ? "bg-neon-green shadow-[0px_0px_15px_3px_var(--color-neon-green)]" : "color-slide color-slide-blue-blue"}`} href={href}>
-              {text}
-              <Icon size={20} />
-            </Link>
-          </li>
-        )}
-      </ul>
-    </nav>
+    // desktop
+    <>
+      <nav className="hidden lg:flex items-center justify-around w-screen border-b-1 border-neon-green p-2">
+        <span className="font-black text-4xl">NexTyper</span>
+        <ul className="flex justify-center gap-8">
+          {links.map(({ text, pathName, href, icon: Icon, key }) =>
+            <li className="flex items-center justify-center" key={key}>
+              <Link className={`flex items-center text-background justify-center gap-2 px-4 py-1 rounded-sm font-medium ${pathname === pathName ? "bg-neon-green shadow-[0px_0px_15px_3px_var(--color-neon-green)]" : "color-slide color-slide-blue-blue"}`} href={href}>
+                {text}
+                <Icon size={20} />
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+      {/* mobile */}
+      <nav className="flex lg:hidden items-center justify-around w-screen border-b-1 border-neon-green p-2 relative">
+        <span className="font-black text-4xl">NexTyper</span>
+        <button onClick={() => setMenuIsVisible(!menuIsVisible)} className="border-2 border-dark-ice rounded-sm px-4 py-2">Menu</button>
+        <ul className={`absolute flex flex-col bg-background justify-center gap-8 py-4 px-2 border-b-1 border-l-1 border-t-1 border-neon-green top-full ${menuIsVisible === true ? "right-0" : "right-100"}`}>
+          {links.map(({ text, pathName, href, icon: Icon, key }) =>
+            <li className="flex items-center justify-center " key={key}>
+              <Link className={`flex items-center text-background justify-center min-w-full gap-2 px-4 py-1 rounded-sm font-medium ${pathname === pathName ? "bg-neon-green shadow-[0px_0px_15px_3px_var(--color-neon-green)]" : "color-slide color-slide-blue-blue"}`} href={href}>
+                {text}
+                <Icon size={20} />
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </>
+
+
   )
 }
 
